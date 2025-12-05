079fe432fbe3f74ed8756708f0c3f5b1d64f1267
ajeebtech <jatintheboy@gmail.com>
2025-12-06 03:04:07 +0530

    ipod 2d

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Ipod } from "@/components/Ipod";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Ipod />
+    </main>
+  );
+}
